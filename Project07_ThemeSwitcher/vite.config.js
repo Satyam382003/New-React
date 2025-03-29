1d81f4ec7b0e0ff44800762737fe6ba9070bf4ff
Satyam <satyampandey382003@gmail.com>
2025-03-29 16:14:38 +0530

    updated

---
+++ Project07_ThemeSwitcher/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+// darkMode: "class";
+export default defineConfig({
+  // darkMode: "class",
+  plugins: [
+    tailwindcss(),
+  ],
+})
